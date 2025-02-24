6cdb95c9208879116d5ae6e7da9d38a78cbed7dd
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-02-24 11:38:13 -0600

    se atualizan rutas

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,15 +1,15 @@
 import { Router } from 'express'
 import { registerUser, editUser, deleteUser} from './user.controller.js'
-import { validateJwt } from '../../middlewares/validate.jwt.js'
+import { validateJwt,isAdmin } from '../../middlewares/validate.jwt.js'
 import { addUserAdminValidator, updateUserAdminValidator } from '../../helpers/validators.js'
 
 
 
 const api = Router()
 
-api.post('/add',[validateJwt,addUserAdminValidator],registerUser)
+api.post('/add',[validateJwt,isAdmin, addUserAdminValidator],registerUser)
 
-api.put('/update',[validateJwt, updateUserAdminValidator],editUser)
+api.put('/update',[validateJwt, isAdmin, updateUserAdminValidator],editUser)
 
 api.delete('/delete',[validateJwt],deleteUser)
 
