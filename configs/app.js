3c6e456d8cd3cc7bb0c00a224630a2579a763939
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-02-26 23:27:26 -0600

    se agregan rutas

--- configs/app.js
+++ configs/app.js
@@ -9,6 +9,7 @@ import { limiter } from '../middlewares/rate.limit.js'
 import categoryRoutes from '../src/category/category.routes.js'
 import productRoutes from '../src/product/product.routes.js'
 import userRoutes from '../src/user/user.routes.js'
+import cartRoutes from '../src/cart/cart.routes.js'
 import  {createDefaultAdmin} from '../configs/setUpData.js'
 
 const configs = (app)=>{
@@ -25,6 +26,7 @@ const routes = (app)=>{
     app.use('/v1/category', categoryRoutes)
     app.use('/v1/product', productRoutes)
     app.use('/v1/user', userRoutes)
+    app.use('/v1/cart', cartRoutes)
 
 }
 
