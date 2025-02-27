77af53567666491d1d33d395a8edbe7725f01856
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-02-26 23:28:07 -0600

    se agregan funciones

--- src/product/product.controller.js
+++ src/product/product.controller.js
@@ -21,7 +21,7 @@ export const getAllProducts = async (req, res) => {
             status: {$ne : false},
             stock: { $ne: 0}
 
-         }).populate('category', 'name -_id')
+        }).populate('category', 'name -_id')
         if(products.length === 0) return res.status(400).send({message: 'Products not found', succes: false})
             return res.send({message: 'Products found',products, succes: true})
     } catch (err) {
@@ -29,8 +29,6 @@ export const getAllProducts = async (req, res) => {
         return res.status(500).json({message: 'General error', succes: false })
     }
 }
-
-
 
 export const getProductById = async (req, res) => {
     try {
@@ -64,7 +62,7 @@ export const updateProduct = async (req, res) => {
 export const deleteProduct = async (req, res) => {
     try {
         const id = req.body.id
-      
+    
         const deletedProduct = await Product.findByIdAndUpdate(id, {status: false})
         if (!deletedProduct) return res.status(404).send({message:'Product not found', success: false})
 
@@ -87,3 +85,38 @@ export const getOutOfStock = async (req, res) => {
     }
 }
 
+export const productByName = async (req, res) => {
+    try {
+        const name = req.body.name
+        
+        const products = await Product.find({
+            name: { $regex: name, $options: 'i' }, 
+            status: { $ne: false },
+            stock: { $gt: 0 }
+        }).populate('category', 'name')
+        if (products.length === 0) return res.status(404).send({ message: 'No products found', success: false })
+
+            return res.send({ message: 'Products found', products, success: true })
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({message: 'General error', success: false})
+    }
+}
+
+export const productByCategory = async (req, res) =>{
+    try {
+        const category = req.body.category
+        const products = await Product.find({
+            category: category,
+            status: { $ne: false },
+            stock: { $gt: 0 }
+        }).populate('category', 'name')
+
+        if(products.length === 0) return res.status(404).send({message: 'No products found', success: false})
+
+            return res.send({message: 'Products found', products, success: true})
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({message: 'General error', success: false})
+    }
+}
