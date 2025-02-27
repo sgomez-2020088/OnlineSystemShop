6cfdff0a7c0158ecbb64eaa991117fb2166a78b0
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-02-26 23:29:08 -0600

    se actualiza modelo

--- src/product/product.model.js
+++ src/product/product.model.js
@@ -1,4 +1,4 @@
-import {Schema, model, Types} from 'mongoose'
+    import {Schema, model, Types} from 'mongoose'
 import Category from '../category/category.model.js'
 
 const productSchema = Schema({
