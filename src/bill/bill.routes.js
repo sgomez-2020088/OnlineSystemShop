34952dcf516390ce0228599922e3960537647d33
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-03-01 07:58:29 -0600

    se agregan rutas

--- src/bill/bill.routes.js
+++ src/bill/bill.routes.js
@@ -1,13 +1,14 @@
 import { Router } from 'express'
 import { generateBill, getUserBills } from '../bill/bill.controller.js'
 import { validateJwt, isAdmin } from '../../middlewares/validate.jwt.js'
+import { userBillValidator } from '../../helpers/validators.js'
 
 const api = Router()
 
 // Confirmar compra y generar factura
 api.get('/bill',[ validateJwt], generateBill)
 
-api.post('/client',[validateJwt, isAdmin],getUserBills)
+api.post('/client',[validateJwt, isAdmin, userBillValidator],getUserBills)
 
 
 export default api
