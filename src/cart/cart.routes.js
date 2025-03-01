2721edf02e292ac68ba870c10a293650c7e3a803
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-03-01 07:58:48 -0600

    se actualizan rutas

--- src/cart/cart.routes.js
+++ src/cart/cart.routes.js
@@ -2,10 +2,11 @@ import { Router } from 'express'
 import { getCart, addCart} from '../cart/cart.controller.js'
 import { validateJwt } from '../../middlewares/validate.jwt.js'
 import { checkStock } from '../../middlewares/stock.js'
+import { addCartValidator } from '../../helpers/validators.js'
 
 const api = Router()
 
 
-api.post('/add', [validateJwt, checkStock], addCart) 
+api.post('/add', [validateJwt, checkStock, addCartValidator], addCart) 
 api.get('/get', [validateJwt], getCart)
 export default api
