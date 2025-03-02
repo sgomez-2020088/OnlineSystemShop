5c21639399f3a36601eee4afae8d86f74d129bce
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-03-02 06:46:41 -0600

    se actualizan rutas

--- src/bill/bill.routes.js
+++ src/bill/bill.routes.js
@@ -1,14 +1,15 @@
 import { Router } from 'express'
-import { generateBill, getUserBills } from '../bill/bill.controller.js'
+import { generateBill, getUserBills, updateBill } from '../bill/bill.controller.js'
 import { validateJwt, isAdmin } from '../../middlewares/validate.jwt.js'
 import { userBillValidator } from '../../helpers/validators.js'
 
 const api = Router()
 
-// Confirmar compra y generar factura
 api.get('/bill',[ validateJwt], generateBill)
 
 api.post('/client',[validateJwt, isAdmin, userBillValidator],getUserBills)
+
+api.put('/update', [validateJwt], updateBill)
 
 
 export default api
