6511d95fe4f9894e0df1f3ea6d1cd3975e2bfef6
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-03-02 20:54:50 -0600

    se actualiza modelo para manejar arreglos

--- src/cart/cart.model.js
+++ src/cart/cart.model.js
@@ -1,33 +1,38 @@
-import {Schema, model, Types} from 'mongoose'
+import { Schema, model, Types } from 'mongoose'
 
-const cartSchema = Schema({
-    user: {
-        type: Types.ObjectId,
-        ref: 'User',    
-        required: [true, 'User is required']
-    },
-    produts:[
-        {
-            product: {
-                type: Types.ObjectId,
-                ref: 'Product',
-                required: [true, 'Product is required']
-            },
-            quantity: {
-                type: Number,
-                default: 1
-            },
-            totalCart:{
-                type: Number,
+const cartSchema = Schema(
+    {
+        user: {
+            type: Types.ObjectId,
+            ref: 'User',
+        },
+        products: [
+            {
+                product: {
+                    type: Types.ObjectId,
+                    ref: 'Product',
+                    required: [true, 'Product is required']
+                },
+                quantity: {
+                    type: Number,
+                    required: [true, 'quantity is required'], 
+                },
+                price: {
+                    type: Number,
+                    required: [true, 'Price is required']
+                }
             }
+        ],
+        total: {
+            type: Number,
+            default: 0
         }
-    ]
-})
+    }
+)
 
-cartSchema.methods.toJSON = function (){
-    const {__v, _id, ...cart } = this.toObject()
+cartSchema.methods.toJSON = function () {
+    const { __v, _id, ...cart } = this.toObject()
     return cart
 }
 
-export default model('Cart', cartSchema)
-
+export default model('Cart', cartSchema);
