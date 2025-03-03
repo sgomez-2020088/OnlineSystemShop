72c0e48cd151ea5a92646c11d3ea175662908e76
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-03-02 20:53:30 -0600

    se actualizan errores

--- src/bill/bill.routes.js
+++ src/bill/bill.routes.js
@@ -1,15 +1,23 @@
 import { Router } from 'express'
-import { generateBill, getUserBills, updateBill } from '../bill/bill.controller.js'
+import { generateBill, getUserBill, updateBill, getHistoryUser} from '../bill/bill.controller.js'
 import { validateJwt, isAdmin } from '../../middlewares/validate.jwt.js'
 import { userBillValidator } from '../../helpers/validators.js'
+import { billData, updateStock, UserBills,formatUserBill,userHistoryBill,formatHistoryBill } from '../../middlewares/lowCut.js'
+
 
 const api = Router()
 
-api.get('/bill',[ validateJwt], generateBill)
+api.get('/bill',[ validateJwt,billData,updateStock], generateBill)
 
-api.post('/client',[validateJwt, isAdmin, userBillValidator],getUserBills)
+api.post('/client',[validateJwt, isAdmin, userBillValidator, UserBills, formatUserBill], getUserBill)
 
-api.put('/update', [validateJwt], updateBill)
+api.put('/update',[validateJwt, isAdmin],updateBill)
+
+api.get('/history',[validateJwt, userHistoryBill, formatHistoryBill], getHistoryUser)
+
+
+
+
 
 
 export default api
