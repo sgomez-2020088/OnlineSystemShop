7c71bef59a09d3b96f54248e0300fa9f123dddfe
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-03-03 09:04:18 -0600

    se agrega funcion updateCart

--- src/cart/cart.controller.js
+++ src/cart/cart.controller.js
@@ -54,3 +54,48 @@ export const addCart = async (req, res) => {
 
 
 
+export const updateCart = async (req, res) => {
+    try {
+        const { productId, quantity } = req.body
+        const userId = req.user.id
+
+        let cart = await Cart.findOne({ user: userId })
+        if (!cart) {
+            return res.status(404).send({ message: 'Cart not found', success: false })
+        }
+
+        const productIndex = cart.products.findIndex(
+            (item) => item.product.toString() === productId
+        )
+
+        if (productIndex === -1) {
+            return res.status(404).send({ message: 'Product not in cart', success: false })
+        }
+
+        if (quantity <= 0) {
+            cart.products.splice(productIndex, 1)
+        } else {
+            cart.products[productIndex].quantity = quantity
+        }
+
+        cart.total = cart.products.reduce(
+            (total, item) => total + item.price * item.quantity,
+            0
+        )
+
+        await cart.save()
+
+        cart = await Cart.findOne({ user: userId })
+            .populate('products.product', 'name description')
+            .populate('user', 'name email -_id')
+
+        return res.status(200).send({
+            message: 'Cart updated successfully',
+            success: true,
+            cart
+        })
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({ message: 'General error', success: false })
+    }
+}
