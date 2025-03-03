17687a559b72329b78efadeacd34810b0b717ad8
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-03-03 09:02:29 -0600

    se agrega validacin jwt

--- middlewares/validate.jwt.js
+++ middlewares/validate.jwt.js
@@ -7,6 +7,8 @@ export const validateJwt = async(req, res, next)=>{
         let secretKey = process.env.SECRET_KEY
         let { authorization } = req.headers
         if(!authorization) return res.status(401).send({message: 'Unauthorized'})
+       
+
         let user = jwt.verify(authorization, secretKey)
         const validateUser = await findUser(user.uid)
         if(!validateUser) return res.status(404).send(
